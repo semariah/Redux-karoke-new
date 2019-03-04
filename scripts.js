7114132d814d4fc66bd54428661ed4ed5ba5bf25
sk <semaria.eyzac@gmail.com>
2019-03-04 15:45:47 -0800

    create a playlist using redux

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+//lyric info
+const songLyricsArray = "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye".split(', ');
+
+//Intial redux state
+
+const initialState = {
+  songLyricsArray: songLyricsArray,
+  arrayPosition: 0,
+}
+
+
+
+//click listner
 const userClick = () => {
   console.log('click');
 }
