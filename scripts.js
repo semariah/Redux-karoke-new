4dffff38530a086e2ac969ab9d0cb60a7b9721e6
sk <semaria.eyzac@gmail.com>
2019-03-04 16:18:38 -0800

    initialize the file using npm init and add the reducer and the state

--- scripts.js
+++ scripts.js
@@ -2,12 +2,16 @@
 const songLyricsArray = "Don't want to be a fool for you, Just another player in your game for two, You may hate me but it ain't no lie, Baby bye bye bye, Bye bye, I Don't want to make it tough, I just want to tell you that I've had enough, It might sound crazy but it ain't no lie, Baby bye bye bye".split(', ');
 
 //Intial redux state
-
 const initialState = {
   songLyricsArray: songLyricsArray,
   arrayPosition: 0,
 }
 
+
+//Redux Store
+const { createStore } = Redux
+const store = createStore(reducer);
+console.log(store.getState());
 
 
 //click listner
